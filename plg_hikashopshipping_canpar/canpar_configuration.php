ac2de4b611b9e6315ac0ffb154c9b100f5c1d60d
Nicolas <hikashop-nicolas@users.noreply.github.com>
2022-03-09 18:32:11 +0100

    Fixed an error on PHP8
    We fixed an error with the Canpar shipping plugin on PHP8
    https://www.hikashop.com/forum/shipping-methods/904088-canpar-shipping-error.html#339999

--- plg_hikashopshipping_canpar/canpar_configuration.php
+++ plg_hikashopshipping_canpar/canpar_configuration.php
@@ -65,48 +65,50 @@ defined('_JEXEC') or die('Restricted access');
 					</thead>
 					<tbody id="warehouse_listing">
 						<?php
-						$a = @count($this->element->shipping_params->warehouse);
-						if($a){
-							for($i = 0;$i<$a;$i++){
-								$row =& $this->element->shipping_params->warehouse[$i];
-								?>
-								<tr class="row0" id="warehouse_<?php echo $i;?>">
-									<td>
-										<input size="10" type="text" id="warehouse_<?php echo $i;?>_name" name="warehouse[<?php echo $i;?>][name]" value="<?php echo @$row->name; ?>"/>
-									</td>
-									<td>
-										<div id="warehouse_<?php echo $i;?>_zip">
-										<input size="10" type="text" id="warehouse_<?php echo $i;?>_zip_input" name="warehouse[<?php echo $i;?>][zip]" value="<?php echo @$row->zip; ?>"/>
-										</div>
-									</td>
+						if(!empty($this->element->shipping_params->warehouse) && is_array($this->element->shipping_params->warehouse)) {
+							$a = count($this->element->shipping_params->warehouse);
+							if($a){
+								for($i = 0;$i<$a;$i++){
+									$row =& $this->element->shipping_params->warehouse[$i];
+									?>
+									<tr class="row0" id="warehouse_<?php echo $i;?>">
+										<td>
+											<input size="10" type="text" id="warehouse_<?php echo $i;?>_name" name="warehouse[<?php echo $i;?>][name]" value="<?php echo @$row->name; ?>"/>
+										</td>
+										<td>
+											<div id="warehouse_<?php echo $i;?>_zip">
+											<input size="10" type="text" id="warehouse_<?php echo $i;?>_zip_input" name="warehouse[<?php echo $i;?>][zip]" value="<?php echo @$row->zip; ?>"/>
+											</div>
+										</td>
 
-									<td align="center">
-										<span id="warehouse_<?php echo $i;?>_zone">
-											<?php if(!empty($row->zone_name)){ echo $row->zone_name;} ?>
-											<input type="hidden" name="warehouse[<?php echo $i;?>][zone]" value="<?php echo @$row->zone ?>"/>
-										</span>
-										<a class="modal" rel="{handler: 'iframe', size: {x: 760, y: 480}}" href="<?php echo hikashop_completeLink("zone&task=selectchildlisting&type=shipping&subtype=warehouse_".$i."_zone&map=warehouse[".$i."][zone]&tmpl=component"); ?>" >
-											<img src="<?php echo HIKASHOP_IMAGES; ?>edit.png"/>
-										</a>
-									</td>
-									<td align="center">
-										<a href="#" onclick="return deleteZone('warehouse_<?php echo $i;?>_zone');">
-											<img src="../media/com_hikashop/images/delete.png"/>
-										</a>
-									</td>
-									<td>
-										<select id="warehouse_<?php echo $i;?>_units"  name="warehouse[<?php echo $i;?>][units]">
-											<option <?php if(@$row->units=='lb')  echo "selected=\"selected\""; ?> value="lb">LB/IN</option>
-											<option <?php if(@$row->units=='kg')  echo "selected=\"selected\""; ?> value="kg">KG/CM</option>
-										</select>
-									</td>
-									<td align="center">
-										<a href="#" onclick="return deleteRow('warehouse_<?php echo $i;?>_zip','warehouse_<?php echo $i;?>_zip_input','warehouse_<?php echo $i;?>');">
-											<img src="../media/com_hikashop/images/delete.png"/>
-										</a>
-									</td>
-								</tr>
-							<?php
+										<td align="center">
+											<span id="warehouse_<?php echo $i;?>_zone">
+												<?php if(!empty($row->zone_name)){ echo $row->zone_name;} ?>
+												<input type="hidden" name="warehouse[<?php echo $i;?>][zone]" value="<?php echo @$row->zone ?>"/>
+											</span>
+											<a class="modal" rel="{handler: 'iframe', size: {x: 760, y: 480}}" href="<?php echo hikashop_completeLink("zone&task=selectchildlisting&type=shipping&subtype=warehouse_".$i."_zone&map=warehouse[".$i."][zone]&tmpl=component"); ?>" >
+												<img src="<?php echo HIKASHOP_IMAGES; ?>edit.png"/>
+											</a>
+										</td>
+										<td align="center">
+											<a href="#" onclick="return deleteZone('warehouse_<?php echo $i;?>_zone');">
+												<img src="../media/com_hikashop/images/delete.png"/>
+											</a>
+										</td>
+										<td>
+											<select id="warehouse_<?php echo $i;?>_units"  name="warehouse[<?php echo $i;?>][units]">
+												<option <?php if(@$row->units=='lb')  echo "selected=\"selected\""; ?> value="lb">LB/IN</option>
+												<option <?php if(@$row->units=='kg')  echo "selected=\"selected\""; ?> value="kg">KG/CM</option>
+											</select>
+										</td>
+										<td align="center">
+											<a href="#" onclick="return deleteRow('warehouse_<?php echo $i;?>_zip','warehouse_<?php echo $i;?>_zip_input','warehouse_<?php echo $i;?>');">
+												<img src="../media/com_hikashop/images/delete.png"/>
+											</a>
+										</td>
+									</tr>
+								<?php
+								}
 							}
 						}
 						?>
